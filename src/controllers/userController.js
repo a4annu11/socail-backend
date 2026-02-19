3e65f9513928427e7630be42d64c9b8eb28c138b
Anurag Solanki <anuragsolanki.idealittechno@gmail.com>
2026-02-19 16:09:00 +0530

    issue fix new 2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,20 +6,15 @@ import Follow from "../models/Follow.js";
 
 export const getMyProfile = async (req, res) => {
   try {
-    const userId = req.user.id;
-
-    const user = await User.findById(userId).select("-password");
-    // .populate("followers following", "username name profilePicture");
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    res.status(200).json({ success: true, user });
+    res.status(200).json({
+      success: true,
+      user: req.user,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
 export const getUserProfile = async (req, res) => {
   try {
     const { username } = req.params;
